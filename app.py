1402248f0707f9c5db9977a8a8acf8bc94059063
Nicolas <nicolas.oulianov@gmail.com>
2022-01-10 15:05:04 +0100

    Edit for live update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import prod, product
 import streamlit as st
 import pandas as pd
 import re
@@ -71,20 +72,46 @@ def compute_environmental_cost(composition: List[str], df_ekpi_perkg: pd.DataFra
 
 df_ekpi_perkg, material_suggestions = load_df()
 
-st.sidebar.markdown("# Compute environmental cost of an existing product")
-try:
+
+def load_product_df():
     product_df = pd.read_csv("data/products.csv")
+    product_df = product_df[["product_name", "composition", "img_path"]]
     product_list = product_df["product_name"].to_list()
-except:
-    product_df = None
-    product_list = []
+    logger.info(product_df)
+    return product_df, product_list
+
+
+st.sidebar.markdown("# Compute environmental cost of an existing product")
+product_df, product_list = load_product_df()
 current_product_name = st.sidebar.selectbox("Select product", product_list)
-if type(product_df) != type(None):
-    current_product = product_df[
-        product_df["product_name"] == current_product_name
-    ].iloc[0]
-else:
-    current_product = None
+
+
+def add_product():
+    product_df = pd.read_csv("data/products.csv")
+    max_i = product_df.index.max() + 1
+    test_df = pd.DataFrame(
+        {
+            "product_name": [f"Product #{max_i}"],
+            "composition": [None],
+            "img_path": "data/img/placeholder.png",
+        }
+    )
+    logger.info("Add product")
+    product_df = product_df.append(test_df).reset_index()
+    product_df.to_csv("data/products.csv")
+
+
+new_product_create = st.sidebar.button(
+    "Create new product",
+    on_click=add_product,
+)
+
+current_product = product_df[product_df["product_name"] == current_product_name]
+logger.info(current_product)
+current_product_index, current_product = (
+    current_product.index,
+    current_product.iloc[0],
+)
 
 
 @st.cache
@@ -93,13 +120,18 @@ def load_image(image_file):
     return img
 
 
-def add_product(product_name: str, composition: List[str], uploadedfile):
+def save_product(
+    index: int,
+    product_name: str,
+    composition: List[str],
+    img_path,
+    uploadedfile,
+):
+    product_df = pd.read_csv("data/products.csv")
     if uploadedfile:
         img_path = f"data/img/{uploadedfile.name}"
         with open(img_path, "wb") as f:
             f.write(uploadedfile.getbuffer())
-    else:
-        img_path = None
     new_product_line = pd.DataFrame(
         {
             "product_name": [product_name],
@@ -107,45 +139,43 @@ def add_product(product_name: str, composition: List[str], uploadedfile):
             "img_path": [img_path],
         }
     )
-    try:
-        df = pd.read_csv("data/products.csv")
-        df = df.append(new_product_line)
-    except:
-        df = new_product_line
-    df.to_csv("data/products.csv")
+    product_df.iloc[index] = new_product_line
+    logger.info("Index:")
+    logger.info(product_df.iloc[index])
+    logger.info("Product")
+    logger.info(new_product_line)
+    product_df.to_csv("data/products.csv")
+    return product_df
 
 
 st.markdown("# Compute the product's environmental cost")
-if type(current_product) != type(None):
+if isinstance(current_product["composition"], str):
     current_product["composition"] = ast.literal_eval(current_product["composition"])
-    col1, col2 = st.columns(2)
-    with col1:
-        product_name = st.text_input(
-            "Product name", value=current_product["product_name"]
-        )
-        st.image(load_image(current_product["img_path"]), width=250)
-        uploadedfile = st.file_uploader("Upload product picture", type=["jpg", "png"])
-    with col2:
-        composition = st_tags(
-            label="Composition (Material:Weight)",
-            value=current_product["composition"],
-            suggestions=material_suggestions,
-        )
-        st.markdown(
-            f"""## Environmental cost: \
-            {compute_environmental_cost(composition, df_ekpi_perkg)}$"""
-        )
-
-    # Add : picture of the product (if available)
-    # Add : histogram of index score
-    # Add : "this product is among the top-1% of sustainable products" (compute quantile)
-
-st.sidebar.button(
-    "Add product",
-    on_click=add_product,
-    kwargs={
-        "product_name": product_name,
-        "composition": composition,
-        "uploadedfile": uploadedfile,
-    },
+else:
+    current_product["composition"] = None
+col1, col2 = st.columns(2)
+with col1:
+    product_name = st.text_input("Product name", value=current_product["product_name"])
+    st.image(load_image(current_product["img_path"]), width=250)
+    uploadedfile = st.file_uploader("Upload product picture", type=["jpg", "png"])
+with col2:
+    composition = st_tags(
+        label="Composition (Material:Weight)",
+        value=current_product["composition"],
+        suggestions=material_suggestions,
+    )
+    st.markdown(
+        f"""## Environmental cost: \
+        {compute_environmental_cost(composition, df_ekpi_perkg)}$"""
+    )
+product_df = save_product(
+    current_product_index,
+    product_name,
+    composition,
+    current_product["img_path"],
+    uploadedfile,
 )
+
+# Add : picture of the product (if available)
+# Add : histogram of index score
+# Add : "this product is among the top-1% of sustainable products" (compute quantile)
